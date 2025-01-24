0e2fa280b8b516b8d387e751dde4fa7e5217f1a3
agayan <a.agayan95@mail.ru>
2025-01-25 00:42:17 +0300

    add full Realization n2

--- commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/WarehouseMapper.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/WarehouseMapper.java
@@ -2,10 +2,15 @@ package ru.yandex.practicum.mapper;
 
 
 import org.mapstruct.Mapper;
+import ru.yandex.practicum.dto.DimensionDto;
 import ru.yandex.practicum.dto.NewProductInWarehouseDto;
 import ru.yandex.practicum.model.WarehouseProduct;
+
+import java.awt.*;
 
 @Mapper
 public interface WarehouseMapper {
     WarehouseProduct toWarehouse(NewProductInWarehouseDto request);
+
+    Dimension toDimension(DimensionDto dimensionDto);
 }
