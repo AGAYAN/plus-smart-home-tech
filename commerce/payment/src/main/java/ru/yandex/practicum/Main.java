bbae14c91ce9b9f59e4904d6f4662a22057eb9c4
agayan <a.agayan95@mail.ru>
2025-02-14 20:42:05 +0400

    fix mistake number 4.2

--- commerce/payment/src/main/java/ru/yandex/practicum/Main.java
+++ commerce/payment/src/main/java/ru/yandex/practicum/Main.java
@@ -3,9 +3,13 @@ package ru.yandex.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import ru.yandex.practicum.delivery.controller.DeliveryClient;
+import ru.yandex.practicum.order.controller.OrderClient;
 
 @SpringBootApplication
 @EnableEurekaServer // or client
+@EnableFeignClients(clients = {DeliveryClient.class, OrderClient.class})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
